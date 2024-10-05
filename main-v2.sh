a04956df94d4c132b6012551e6d788594366a1ed
PabloTzy <135358752+PabloTzy@users.noreply.github.com>
2024-10-05 16:54:18 +0700

    Update main-v2.sh

--- main-v2.sh
+++ main-v2.sh
@@ -13,13 +13,21 @@ read -p "Masukkan pilihan Anda [1/2]: " opsi
 sudo apt update
 sudo apt install -y nginx mysql-server php8.1-fpm php8.1-mysql wget unzip
 
-# Install phpMyAdmin
-cd /var/www/html
-wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
-unzip phpMyAdmin-latest-all-languages.zip
-mv phpMyAdmin-*-all-languages pma
-rm phpMyAdmin-latest-all-languages.zip
-sudo chown -R www-data:www-data /var/www/html/pma
+# Buat direktori untuk phpMyAdmin
+mkdir -p /var/www/phpmyadmin/tmp && cd /var/www/phpmyadmin
+
+# Unduh dan ekstrak phpMyAdmin
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
+tar xvzf phpMyAdmin-latest-english.tar.gz
+mv phpMyAdmin-*-english/* /var/www/phpmyadmin
+rm phpMyAdmin-latest-english.tar.gz
+
+# Ubah kepemilikan dan izin
+chown -R www-data:www-data /var/www/phpmyadmin
+mkdir /var/www/phpmyadmin/config
+chmod o+rw /var/www/phpmyadmin/config
+cp /var/www/phpmyadmin/config.sample.inc.php /var/www/phpmyadmin/config/config.inc.php
+chmod o+w /var/www/phpmyadmin/config/config.inc.php
 
 # Konfigurasi Nginx berdasarkan pilihan HTTP atau HTTPS
 if [ "$opsi" -eq 1 ]; then
@@ -27,88 +35,122 @@ if [ "$opsi" -eq 1 ]; then
     sudo apt install -y certbot python3-certbot-nginx
 
     # Konfigurasi Nginx dengan HTTPS
-    cat <<EOL | sudo tee /etc/nginx/sites-available/$domain.conf
+    cat <<EOL | sudo tee /etc/nginx/sites-available/phpmyadmin.conf
 server {
     listen 80;
     server_name $domain;
+    return 301 https://\$server_name\$request_uri;
+}
 
-    root /var/www/html;
+server {
+    listen 443 ssl http2;
+    server_name $domain;
+
+    root /var/www/phpmyadmin;
     index index.php;
 
-    location /pma/ {
-        alias /var/www/html/pma/;
-        index index.php;
-        
-        location ~ \.php\$ {
-            include snippets/fastcgi-php.conf;
-            fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
-        }
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+    sendfile off;
 
-        location ~ /\.ht {
-            deny all;
-        }
-    }
+    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+    ssl_session_cache shared:SSL:10m;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers 'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
+    ssl_prefer_server_ciphers on;
+
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
 
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php\$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\.ht {
+        deny all;
     }
 }
 EOL
 
     # Aktifkan konfigurasi Nginx dan jalankan Certbot untuk HTTPS
-    sudo ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
+    sudo ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/phpmyadmin.conf
     sudo systemctl restart nginx
     sudo certbot --nginx -d $domain --non-interactive --agree-tos --email admin@$domain
 
 elif [ "$opsi" -eq 2 ]; then
     # Konfigurasi Nginx tanpa HTTPS
-    cat <<EOL | sudo tee /etc/nginx/sites-available/$domain.conf
+    cat <<EOL | sudo tee /etc/nginx/sites-available/phpmyadmin.conf
 server {
     listen 80;
     server_name $domain;
 
-    root /var/www/html;
+    root /var/www/phpmyadmin;
     index index.php;
 
-    location /pma/ {
-        alias /var/www/html/pma/;
-        index index.php;
-        
-        location ~ \.php\$ {
-            include snippets/fastcgi-php.conf;
-            fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
-        }
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+    sendfile off;
 
-        location ~ /\.ht {
-            deny all;
-        }
-    }
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
 
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php\$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\.ht {
+        deny all;
     }
 }
 EOL
 
     # Aktifkan konfigurasi Nginx tanpa SSL
-    sudo ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
+    sudo ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/phpmyadmin.conf
     sudo systemctl restart nginx
 else
     echo "Pilihan tidak valid, proses dihentikan."
     exit 1
 fi
 
-# Konfigurasi MySQL bind address
-sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo systemctl restart mysql
-
-# Membuat user admindb dan admin dengan akses root dan remote
-mysql -u root <<MYSQL_SCRIPT
-CREATE USER 'admindb'@'%' IDENTIFIED BY 'password_anda';
-CREATE USER 'admin'@'%' IDENTIFIED BY 'password_anda';
-GRANT ALL PRIVILEGES ON *.* TO 'admindb'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
-
-echo "phpMyAdmin (di /pma), konfigurasi Nginx, dan MySQL user setup completed!"
+echo "phpMyAdmin setup completed!"
